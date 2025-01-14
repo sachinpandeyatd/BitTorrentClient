5181ff3ed95c81ca9fda2e27aa4cab9165d8482d
Sachin Pandey <33sachinedu@gmail.com>
2025-01-14 15:03:51 +0530

    Parse magnet url

--- src/main/java/com/main/MainClass.java
+++ src/main/java/com/main/MainClass.java
@@ -1,6 +1,9 @@
 package com.main;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.util.Map;
 import java.util.Scanner;
 
 public class MainClass {
@@ -8,20 +11,29 @@ public class MainClass {
 		System.out.println("What are you having - \n1) Maganet Link \n2) Torrent File \nJust enter the number");
 		Scanner scanner = new Scanner(System.in);
 		int inputType = scanner.nextInt();
-		scanner.reset();
 
 		TorrentParser torrentParser = new TorrentParser();
 		switch (inputType) {
 		case 1:
-			torrentParser.parseMaganetLink();
-			break;
+			System.out.print("Please enter your magnet link - ");
+			String magnetURL = scanner.nextLine();
+			scanner.close();
+
+            try {
+                Map<String, String> params = torrentParser.parseMagnetLink(magnetURL);
+            } catch (URISyntaxException | UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
+            break;
 
 		case 2:
+			System.out.println("Please enter your torrent file location");
+			String torrFileLocation = scanner.nextLine();
+			scanner.close();
 			try {
-				torrentParser.parseTorrentFile();
+				torrentParser.parseTorrentFile(torrFileLocation);
 			} catch (IOException e) {
 				System.out.println("An exception occured - " + e.getMessage());
-				e.printStackTrace();
 			}
 			break;
 		}
--- src/main/java/com/main/TorrentParser.java
+++ src/main/java/com/main/TorrentParser.java
@@ -2,24 +2,32 @@ package com.main;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLDecoder;
 import java.nio.file.Files;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TorrentParser {
 
-	public void parseMaganetLink() {
-		System.out.print("Please enter your maganet link - ");
-		Scanner scanner = new Scanner(System.in);
-		String maganetURL = scanner.next();
-		scanner.close();
+	public Map<String, String> parseMagnetLink(String magnetURL) throws URISyntaxException, UnsupportedEncodingException {
+		URI uri = new URI(magnetURL);
+		String query = uri.getRawQuery();
+		Map<String, String> params = new HashMap<>();
+
+		for(String pair : query.split("&")){
+			int idx = pair.indexOf('=');
+			String key = URLDecoder.decode(pair.substring(0, idx), "UTF-8");
+			String value = URLDecoder.decode(pair.substring(idx + 1), "UTF-8");
+			params.put(key, value);
+		}
+
+		return params;
 	}
 
-	public void parseTorrentFile() throws IOException {
-		System.out.println("Please enter your torrent file location");
-		Scanner scanner = new Scanner(System.in);
-		String torrFileLocation = scanner.next();
-		scanner.close();
-		
+	public void parseTorrentFile(String torrFileLocation) throws IOException {
 		File torrFile = new File(torrFileLocation);
 		byte[] torrFileData = Files.readAllBytes(torrFile.toPath());
 	}
